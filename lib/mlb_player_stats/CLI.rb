13a2a84c3d8205f167b95238e8e7d8bf94556037
Zachary Durr <zdurr22@gmail.com>
2021-01-17 21:55:50 +0000

    add display_team method

--- lib/mlb_player_stats/CLI.rb
+++ lib/mlb_player_stats/CLI.rb
@@ -3,6 +3,7 @@ class MlbPlayerStats::CLI
   def call
     puts "\nWelcome to MlbPlayerStats!\n"
     get_teams
+    list_teams
     get_user_team
   end
 
@@ -13,20 +14,23 @@ class MlbPlayerStats::CLI
 
   def list_teams
     puts "Select the number associated with the team you'd like to view:"
-    @teams.each_with_index do |team, index|
-      puts "#{index + 1}. #{team}"
+    @teams.each.with_index(1) do |team, index|
+      puts "#{index}. #{team}"
     end
   end
 
   def get_user_team
-    list_teams
-    chosen_team = gets.strip
-    # if valid_input(chosen_team, @teams)
-    #
-    # end
+    chosen_team = gets.strip.to_i
+    display_team(chosen_team) if valid_input(chosen_team, @teams)
   end
 
   def valid_input(input, data)
     input.to_i <= data.length && input.to_i >> 0
+  end
+
+  def display_team(chosen_team)
+    team = @teams[chosen_team - 1]
+    puts "Here are players on the 40-man-roster for #{team}:"
+
   end
 end
