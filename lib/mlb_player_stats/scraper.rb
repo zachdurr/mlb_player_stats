604e788aba2afe629bc127a27c922778a8b136a9
Zachary Durr <zdurr22@gmail.com>
2021-01-17 23:30:58 +0000

    move scraper class to lib

--- lib/mlb_player_stats/scraper.rb
+++ lib/mlb_player_stats/scraper.rb
@@ -2,26 +2,26 @@ class MlbPlayerStats::Scraper
 
 site = "https://www.baseball-reference.com/"
 
-page = Nokogiri::HTML(URI.open(site + "teams/"))
+page = Nokogiri::HTML(open(site + "teams/"))
 
 @teams = page.css(".row_summable").css("tbody tr").css(".left").css("a")
 
-def team_links
+def get_team_links
   @teams.map do |team|
     team["href"]
   end
 end
 
-def team_names
+def get_team_names
   @teams.map do |team|
     team.text
   end
 end
 
-team_page = Nokogiri::HTML(URI.open(site + team_links[4]))
+team_page = Nokogiri::HTML(open(site + team_links[4]))
 
 player_list = team_page.css(".grouplist").css("li").map(&:text)
 
 puts player_list
-puts team_names
+puts get_team_names
 end
