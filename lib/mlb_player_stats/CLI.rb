1ed7d0040ed527aabaa8b96902dfe0383ddc47e4
Zachary Durr <zdurr22@gmail.com>
2021-01-17 20:48:17 +0000

    start cli stub

--- lib/mlb_player_stats/CLI.rb
+++ lib/mlb_player_stats/CLI.rb
@@ -2,7 +2,18 @@ class MlbPlayerStats::CLI
 
   def call
     puts "Welcome to MlbPlayerStats!"
+    get_teams
+    get_user_team
   end
 
+  def get_teams
+    @teams = ["Atlanta Braves", "Arizona Diamondbacks", "Baltimore Orioles"]
+  end
+
+  def get_user_team
+    @teams.each_with_index do |team, index|
+      puts "#{index + 1}. #{team}"
+    end
+  end
 
 end
